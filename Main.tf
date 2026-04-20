e62f1b8dabc587995301358d6692bd502c41f65d
Mohammad.E Eslami <mohamad.slami@gmail.com>
2026-04-19 22:03:37 -0700

    Ver0.2

--- Main.tf
+++ Main.tf
@@ -12,3 +12,5 @@ resource "local_file" "example" {
   filename = "hello.txt"
   content  = "Hello from Terraform 🚀. Saaaa fdfdgdgdgdg Test.  01. dfsfsfsff sdfsfsfsf sfsf"
 }
+sddsfsfsdfdsfsdfsfsfsfsfxcvxcvcxvcxvcxv
+xcvcxvxcvcxvxcv
